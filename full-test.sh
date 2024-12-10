deb352521cdcb41ddca64deccb1326ac2cb6d0af
Andrey Zargarov <andrey.zargarov@gmail.com>
2024-12-10 19:56:00 +0100

    removed comments/pretty printing health check proc

--- full-test.sh
+++ full-test.sh
@@ -24,7 +24,7 @@ command_check() {
 http_health_check() {
   local url=$1
   local expected=$2
-  echo "Trying curl -s $url"
+  echo "Trying -> curl -s $url"
   for i in {1..50}; do
     if curl -s "$url" | grep -q "$expected"; then
       echo "OK"
@@ -33,6 +33,7 @@ http_health_check() {
     echo -n "."
     sleep 1 
   done
+  echo "FAILED"
   return 1 
 }
 
@@ -63,6 +64,7 @@ for test_name in "${!tests[@]}"; do
 done
 
 # Print summary
+echo "==============================="
 for test_name in "${!tests[@]}"; do
   if [ "${results[$test_name]}" == "PASS" ]; then
     echo "${test_name}: ${results[$test_name]}"
@@ -80,33 +82,3 @@ fi
 echo
 
 exit "$failed_counter"
-
-#2 verify helm is installed
-#3 verify docker is installed
-#4 swagger is spun off
-#5 verivy ceph-pool pvc, ceph-cluster, ceph status
-#6 Verify grafana, promtail, loki are availiable 
-
-#kubectl -n rook-ceph exec -it $( kubectl get pods -n rook-ceph -l app=rook-ceph-tools -o jsonpath='{.items[0].metadata.name}') -- bash
-# kubectl -n rook-ceph exec -it $(kubectl -n rook-ceph get pod -l app=rook-ceph-tools -o jsonpath='{.items[0].metadata.name}') -- bash
-# ceph osd status
-# ceph osd tree
-# 
-#  kubectl -n rook-ceph get cephblockpool replicapool
-# NAME          PHASE   TYPE         FAILUREDOMAIN   AGE
-# replicapool   Ready   Replicated   host            20m
-# 
-#  kubectl get pvc rook-ceph-pvc
-# NAME            STATUS   VOLUME                                     CAPACITY   ACCESS MODES   STORAGECLASS      VOLUMEATTRIBUTESCLASS   AGE
-# rook-ceph-pvc   Bound    pvc-d3fc2e36-3e0e-4fd4-8720-43e9175a97bd   1Gi        RWO            rook-ceph-block   <unset>                 20m
-# 
-# kubectl -n rook-ceph get cephcluster
-# NAME        DATADIRHOSTPATH   MONCOUNT   AGE   PHASE   MESSAGE                        HEALTH        EXTERNAL   FSID
-# rook-ceph   /var/lib/rook     1          64m   Ready   Cluster created successfully   HEALTH_WARN              7eeab713-23fe-4e0e-a98a-c9b7d93af12
-# 
-#  kubectl -n rook-ceph get pods -l app=rook-ceph-osd
-# NAME                               READY   STATUS    RESTARTS   AGE
-# rook-ceph-osd-0-57bf5d558d-k7st8   1/1     Running   0          21m
-# rook-ceph-osd-1-7bcbcd74b-msdrc    1/1     Running   0          21m
-# rook-ceph-osd-2-7855cc7b4d-spqmh   1/1     Running   0          21m
-# rook-ceph-osd-3-85c9b749b-lfxpx    1/1     Running   0          20m
