bb96fe79eb95375010defc59f3392e41f48871ce
Andrey Zargarov <andrey.zargarov@gmail.com>
2024-12-17 21:14:08 +0100

    refactor(grafana) Changed installation, ansible->kubectl manifest

--- scripts/monitoring_tools_setup.sh
+++ scripts/monitoring_tools_setup.sh
@@ -3,6 +3,9 @@ set -e
 
 echo ""
 
+echo "Installing Grafana"
+sudo -i -u user kubectl apply -f /mnt/files/manifests/grafana.yaml
+
 echo "Installing Prometheus"
 sudo -i -u user helm install prometheus prometheus-community/prometheus --namespace monitoring
 sudo -i -u user helm upgrade prometheus prometheus-community/prometheus --namespace monitoring --set server.service.type=NodePort --set server.service.nodePort=30900  --set server.service.port=9090 --set server.service.targetPort=9090
