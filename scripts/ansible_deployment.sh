0a5042e3f5839341254200ffd3284e1b3160feca
Andrey Zargarov <andrey.zargarov@gmail.com>
2024-12-17 21:50:47 +0100

    fix(rook-ceph). Changed deployment to kube manifest

--- scripts/ansible_deployment.sh
+++ scripts/ansible_deployment.sh
@@ -2,5 +2,5 @@
 set -e
 echo "Running Ansible playbooks to deploy K3S, Rook-Ceph, Grafana"
 sudo -i -u user ansible-playbook -i /mnt/files/ansible/inventory/hosts.ini /mnt/files/ansible/k3s_lab_setup_new.yaml
-sudo -i -u user ansible-playbook -i /mnt/files/ansible/inventory/hosts.ini /mnt/files/ansible/rook_ceph_install_new.yaml
+#sudo -i -u user ansible-playbook -i /mnt/files/ansible/inventory/hosts.ini /mnt/files/ansible/rook_ceph_install_new.yaml
 echo "Ansible deployments completed."
