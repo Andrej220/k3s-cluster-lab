262d5d855ea114e51e44bb70f857a702db481da9
Andrey Zargarov <andrey.zargarov@gmail.com>
2024-12-22 12:53:55 +0100

    refactor(config) changed grafana installation -> kube manifest
     Changes to be committed:
    	new file:   files/manifests/grafana_datasources_configmap.yaml
    	renamed:    files/manifests/grafana.yaml -> files/manifests/grafana_deployment.yaml
    	new file:   files/manifests/monitoring_namespace.yaml
    	modified:   scripts/monitoring_tools_setup.sh

--- scripts/monitoring_tools_setup.sh
+++ scripts/monitoring_tools_setup.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 set -e
 
-echo ""
+FILESPATH=/mnt/files
+MANIFESTPATH="$FILESPATH"/manifests
 
 echo "Installing Grafana"
-sudo -i -u user kubectl apply -f /mnt/files/manifests/grafana.yaml
+echo "Installing namespace - monitoring"
+sudo -i -u user kubectl apply -f "$MANIFESTPATH"/monitoring_namespace.yaml
+echo "Installing Config map"
+sudo -i -u user kubectl apply -f "$MANIFESTPATH"/grafana_datasources_configmap.yaml
+echo "Deploing grafan"
+sudo -i -u user kubectl apply -f "$MANIFESTPATH"/grafana.yaml
+kubectl rollout status deployment/grafana -n monitoring
 
 echo "Installing Prometheus"
-sudo -i -u user helm install prometheus prometheus-community/prometheus --namespace monitoring
-sudo -i -u user helm upgrade prometheus prometheus-community/prometheus --namespace monitoring --set server.service.type=NodePort --set server.service.nodePort=30900  --set server.service.port=9090 --set server.service.targetPort=9090
+sudo -i -u user helm install prometheus prometheus-community/prometheus --namespace monitoring --wait --timeout 300s
+sudo -i -u user helm upgrade prometheus prometheus-community/prometheus --namespace monitoring --set server.service.type=NodePort --set server.service.nodePort=30900  --set server.service.port=9090 --set server.service.targetPort=9090 --wait --timeout 300s
 
 echo "Installing Loki"
-sudo -i -u user helm upgrade --install loki grafana/loki-distributed -n monitoring -f /mnt/files/loki_values.yaml
+sudo -i -u user helm upgrade --install loki grafana/loki-distributed -n monitoring -f "$FILESPATH"/loki_values.yaml --wait --timeout 300s
 
 echo "Installing Jaeger"
-sudo -i -u user helm upgrade --install jaeger bitnami/jaeger -n monitoring 
+sudo -i -u user helm upgrade --install jaeger bitnami/jaeger -n monitoring  --wait --timeout 300s
 
 echo "Monitoring tools setup completed."
