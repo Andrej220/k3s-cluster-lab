c7bc2557ddb57a9662b9323bdfc3d76cbceb983d
Andrey Zargarov <andrey.zargarov@gmail.com>
2025-01-06 23:53:15 +0100

    feature(ArgoCD) install CLI

--- scripts/argocd_setup.sh
+++ scripts/argocd_setup.sh
@@ -6,5 +6,10 @@ sudo -i -u user kubectl apply -f /mnt/files/manifests/argocd_namespace.yaml
 echo "Installing ArgoCD"
 sudo -i -u user kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-echo "Install loadbalancer"
+echo "Installing loadbalancer"
 sudo -i -u user kubectl apply -f /mnt/files/manifests/argocd-loadbalancer.yaml
+
+echo "Installing ArgoCD CLI"
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
